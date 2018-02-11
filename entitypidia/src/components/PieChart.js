26440dc8d780ba028ea16a200cce161b65c55081
remster85 <remi.desreumaux@gmail.com>
2018-02-11 18:19:51 -0500

    Calculate components contributions

--- entitypidia/src/components/PieChart.js
+++ entitypidia/src/components/PieChart.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {Pie} from 'react-chartjs-2';
 
 
-
 export default class PieChart extends Component {
 
         render() {
@@ -12,9 +11,25 @@ export default class PieChart extends Component {
                 const entityname = this.props.entityname;
                 const filename = entityname + 'composition.json';
                 var values = require('./../jsonsamples/' + filename);
+
+                console.log(values);
                
-                var weights = Object.keys(values).map(function(k) { return values[k]['property5'] });
-                var entitycomponents = Object.keys(values).map(function(k) { return values[k]['property1'] });
+                var groupedByCommos = [];
+
+                values.reduce(function (res, value) {
+                    if (!res[value.property1]) {
+                        res[value.property1] = {
+                            property5: 10,
+                            property1: value.property1
+                        };
+                        groupedByCommos.push(res[value.property1])
+                    }
+                    res[value.property1].property5 += value.property5
+                    return res;
+                }, {});
+                 
+                var weights = Object.keys(groupedByCommos).map(function(k) { return groupedByCommos[k]['property5'] });
+                var entitycomponents = Object.keys(groupedByCommos).map(function(k) { return groupedByCommos[k]['property1'] });
                 
                 var colors = ["red","green","orange","purple","black","pink","white","brown","amazon","amber","auburn","autumn","bazaar","beaver","bone","brandy","buff"];
                 var hoverBackgroundColor = ["#0048BA","#B0BF1A","#7CB9E8","#C9FFE4","#B284BE","black","pink","white","brown"];
